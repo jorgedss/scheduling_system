a65a85a9c8229f8d13ff7c79a998449b1bd22ba9
Jorge Souza <jorgesouza621@gmail.com>
2025-05-03 09:29:54 -0300

    style: add input placeholders and translate conflict message to English

--- scheduling_system/scheduling_system/doctype/appointment/appointment.py
+++ scheduling_system/scheduling_system/doctype/appointment/appointment.py
@@ -38,8 +38,8 @@ class Appointment(Document):
 
 		if conflicts:
 			conflict_dict = conflicts[0]
-			init = format_datetime(conflict_dict["start_date"])
+			start = format_datetime(conflict_dict["start_date"])
 			end = format_datetime(conflict_dict["end_date"])
 
-			frappe.throw(frappe._("Este vendedor está ocupado de {0} às {1}").format(init, end),
-								title=frappe._("Conflito de agendamento"))
+			frappe.throw(frappe._("This seller has an appointment from {0} to {1}. Please choose another time.").format(start, end),
+								title=frappe._("Scheduling conflict"))
